082ce47461313a9d7d043b573deb5ac9ad095072
vivekbhardwajaug07 <vivekbhardwajaug07@gmail.com>
2025-12-19 11:34:21 +0530

    Update MainClass.java

--- DemoOne/src/main/java/com/main/MainClass.java
+++ DemoOne/src/main/java/com/main/MainClass.java
@@ -4,8 +4,9 @@ public class MainClass {
 	
 //this is the main class
 	public static void main(String[] args) {
-		System.out.println("i am qa person");
+		System.out.println("i am qa person****");
 		System.out.println(" test This is the new changes again with new changes!!!!##");
 	}
 }
+
 
